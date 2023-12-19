7f9b922294292215b19f9ec77691f0f343214eda
Danito <dany_rolero@hotmail.com>
2023-12-19 23:44:06 +0100

    Ultimos scripts

--- script.js
+++ script.js
@@ -1,36 +1,17 @@
-//Obtener el nombre del formulario
-//Validar nombre del formulario
-//Resaltar error en el campo nombre del formulario
-
-//Obtener correo del formulario
-//Validar correo
-//Resaltar error en el campo correo del formulario
-
-//Obtener número de personas
-
-//Obtener tipo de escapada elegida
-
-//Calcular el precio de la escapada
-//Mostrar un mensaje en el que aparezca en nombre introducido y el precio total de la escapada
-
-
-
-
-
 function calcularPrecio() {
     //Obtener el nombre
     let nombre = document.getElementById('nombre').value.trim();
+    if(!validarNombre(nombre)) errorNombre();
+
 
     //Obtener el correo
     let correo = document.getElementById('correo').value.trim();
+    if(!validarCorreo(correo)) errorEmail();
 
     //Obtener la cantidad de personas
     let personas = document.getElementById('personas');
-    let numPersonas;
-
-    for(let i=0; i < personas.length; i++) {
-        if(personas[i].selected) numPersonas = personas[i].value;
-    }
+    let index = personas.selectedIndex;
+    let numPersonas = personas[index].value;
 
     //Obtener el tipo de escapada
     let escapadas = document.getElementsByName('escapada');
@@ -46,16 +27,49 @@ function calcularPrecio() {
     precios["montana"] = 130;
     precios["playa"] = 150;
     let precioTotal = precios[escapadaSeleccionada] * numPersonas;
+
+    document.getElementById('precioTotal').innerHTML = nombre + " Disfruta de tu escapada por: " + precioTotal +"€" + "\n";
+
 }
 
+//------------------------------------------------------------------------
 //Validar campo nombre
 function validarNombre(nombre) {
     var regex = /^[a-zA-Z]+(?:\s[a-zA-Z]+)?$/;
-    if(!regex.test(nombre)) //Ejecuta error campo nombre
+    if(!regex.test(nombre)) return false;
+    return true;
 }
 
+//------------------------------------------------------------------------
+//Mostrar error en el formularios
 function validarCorreo(correo) {
+    let regex = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+    if(!regex.test(correo)) return false;
 
-    return false;
+    return true;
+}
+
+//------------------------------------------------------------------------
+//Mostrar error en el campo nombre
+function errorNombre() {
+    let campoNombre = document.getElementById('nombre');
+    campoNombre.classList.add('errorCampo');
+    document.getElementById('errorNombre').style.display = 'block';    
+}
+
+//------------------------------------------------------------------------
+//Mostrar error en el campo email
+function errorEmail() {
+    let campoEmail = document.getElementById('correo');
+    campoEmail.classList.add('errorCampo');
+    document.getElementById('errorEmail').style.display = 'block';    
+}
+
+
+function restablecerCampo(campo) {
+    campo.classList.remove('errorCampo');
+    campo.value = "";
+    let campos = document.getElementsByName('errorCampo');
+    for(let i = 0; i < campos.length; i++) campos[i].style.display = "none";
 }
 
